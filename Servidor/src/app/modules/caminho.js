576dac64a73dede2aec93367610de8801685e89b
villi <vkulkamp@gmail.com>
2019-10-06 12:24:15 -0300

    subindo versão atualizada do servidor com a listagem das pastas

---
+++ Servidor/src/app/modules/caminho.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs')
+//const pathh = __dirname; 
+//const pathh = "../../public";
+
+function lerDir() {
+
+    const pathh = __dirname;   
+    console.log(pathh);
+    const ler = fs.readdirSync(pathh);
+    console.log("dentro do caminhos   " + ler)
+    
+    
+    const pathh2 = "./src/app/public";
+    console.log("teste de caminho " + pathh2)
+    const ler2 = fs.readdirSync(pathh2);
+    console.log("dentro dos caminhas " + ler2)
+
+    
+    return  {Lista: ler2};
+    
+}
+
+module.exports= lerDir;
+
---
+++ Servidor/src/config/custon-express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const rotas = require('../app/rotas/rotas');
+
+app.use('/public', express.static('src/app/public'));
+app.use('/', express.static('src/app/public'));
+
+rotas(app);
+
+module.exports=app;
